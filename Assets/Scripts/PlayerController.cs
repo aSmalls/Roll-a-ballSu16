7d104dd5a5682ac5fe5efe48e95862af57b581a0
TJ Smaldone <tjsmaldone@gmail.com>
2016-06-02 16:04:10 -0400

    Commit 6
    6th Commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void Start(){
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+	        
 
 	void FixedUpdate(){
 		float moveHorizontal = Input.GetAxis("Horizontal");
@@ -17,6 +27,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "PickUp") {
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString();
+		if(count >= 12) {
+			winText.text = "YOU WIN!";
 		}
 	}
 }
